946fabe7c39a4c875e1b3bc058a51e3b256649eb
wei yang <davidyoung8906@gmail.com>
2014-08-01 17:40:24 -0700

    Add tryif

---
+++ JavaTry/src/Tryif.java
@@ -0,0 +1,18 @@
+
+public class Tryif {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 1;
+		int b =2;
+		boolean c = true;
+		if(c = (a > b)){
+			System.out.println("a: "+a+"b: "+b+"c: "+c);
+		}
+		System.out.println("false: "+ "a: "+a+"b: "+b+"c: "+c);
+	}
+
+}
